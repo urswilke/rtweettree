e3bafea9abea9b2826b0667925a5d59c7dbaaba3
urswilke <urs.wilke@gmail.com>
2020-03-03 21:49:40 +0100

    created license

---
+++ R/usethis_etc.R
@@ -0,0 +1,8 @@
+library(usethis)
+library(roxygen2)
+library(devtools)
+
+use_git_config(user.name = "urswilke", user.email = "urs.wilke@gmail.com")
+use_git()
+use_readme_rmd()
+usethis::use_mit_license("Urs Wilke")
