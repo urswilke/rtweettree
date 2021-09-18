9b5356c702ed5097ed6b7bc11ad58e684e1c29b7
Urs Wilke <urs.wilke@gmail.com>
2021-09-18 14:29:13 +0200

    add dist_to_center columns to nodes of graph

--- tests/testthat/test-network.R
+++ tests/testthat/test-network.R
@@ -1,6 +1,7 @@
 suppressMessages(library(tidygraph))
+suppressMessages(library(tidyverse))
 suppressMessages(g <- create_tweet_tbl_graph(df_main_status, df_tree, df_tls, df_favs, df_retweets))
-
+g <- g %>% mutate(dist_to_center = node_distance_from(node_is_source()))
 test_that("generated tbl graph object still the same", {
   expect_snapshot(g %>% as_tibble())
   expect_snapshot(g %E>% as_tibble())
