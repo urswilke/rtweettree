cccf6cee8b08b2f78e665a70dbdacf78cd4fdfff
Urs Wilke <urs.wilke@gmail.com>
2021-09-19 00:13:59 +0200

    update graphical test to use autoplot()

--- tests/testthat/test-network.R
+++ tests/testthat/test-network.R
@@ -2,15 +2,9 @@ suppressMessages(library(tidygraph))
 suppressMessages(library(ggraph))
 suppressMessages(library(dplyr))
 suppressMessages(library(purrr))
-suppressMessages(g <- rtweettree_tbl_graph(tibble::lst(df_main_status, df_tree, df_tls, df_favs, df_retweets)))
-g <- g %>% mutate(dist_to_center = node_distance_from(node_is_source()))
+suppressMessages(l <- rtweettree_data(tibble::lst(df_main_status, df_tree, df_tls, df_favs, df_retweets)))
+suppressMessages(g <- rtweettree_tbl_graph(l))
 
-df_profile_pic$profile_image_url <- 1:3 %>% map_chr(~system.file("profile_pics", paste0(.x, ".png"), package = "rtweettree"))
-df_profile_pic <- df_profile_pic %>%
-  dplyr::mutate(
-    img = profile_image_url %>%
-      purrr::map(magick::image_read)
-  )
 
 test_that("generated tbl graph object still the same", {
   expect_snapshot(g %>% as_tibble())
@@ -18,14 +12,7 @@ test_that("generated tbl graph object still the same", {
 })
 
 test_that("generated ggraph graph object still the same", {
-  ggraph_fun <- add_profile_pics_to_tree_ggraph(
-      g,
-      df_profile_pic
-    ) +
-      geom_edge_diagonal(aes(color= type)) +
-      scale_edge_colour_hue(name = "action") +
-      geom_node_point(aes(color = dist_to_center,
-                          shape = type))
+  suppressMessages(ggraph_fun <- autoplot(l))
   vdiffr::expect_doppelganger(
     "tweet ggraph",
     ggraph_fun
