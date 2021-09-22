76fadb6da727fe5764c9fec43e2e85d725ce0f9d
Urs Wilke <urs.wilke@gmail.com>
2021-09-22 18:14:33 +0200

    rename function create_tweet_tbl_graph() to rtweettree_tbl_graph.rtweettree_data() method

--- R/rtweettree_tbl_graph.R
+++ R/rtweettree_tbl_graph.R
@@ -1,3 +1,4 @@
+# TODO: make sure to correctly auto-link (pkgdown) the inline expressions inside "``"!
 #' Create a tidygraph tbl_graph object
 #'
 #' Create a `tidygraph::tbl_graph` object representing the tree structure of a
@@ -38,31 +39,122 @@ rtweettree_tbl_graph <- function(x, ...) {
   UseMethod("rtweettree_tbl_graph")
 }
 
-#' @export
+#' @describeIn rtweettree_tbl_graph Construct rtweettree_tbl_graph object from rtweettree_data.
 rtweettree_tbl_graph.rtweettree_data <- function(x, ...) {
-  g <-
-    create_tweet_tbl_graph(x)
+  suppressMessages(df <- x %>% purrr::reduce(dplyr::full_join) %>% dplyr::distinct(.data$status_id, .keep_all = TRUE))
+  df_root <-
+    x$df_main_status %>%
+    # df_tree %>%
+    # filter(status_id == main_status_id) %>%
+    dplyr::select(to = .data$status_id, .data$user_id, .data$screen_name) %>%
+    dplyr::mutate(from = "root", type = "root")
+  tweet_edges <-
+    find_connections_rec(dplyr::bind_rows(x$df_tree, x$df_tls, x$df_favs), df_root)
+  user_tweet_edges <-
+    tweet_edges %>%
+    dplyr::bind_rows(df_root %>% dplyr::mutate(from = .data$screen_name)) %>%
+    dplyr::transmute(.data$user_id,
+                     .data$screen_name,
+                     from = .data$to,
+                     to = .data$user_id,
+                     type = "by")
 
+
+  fav_edges <-
+    x$df_favs %>%
+    dplyr::filter(.data$status_id %in% tweet_edges$to) %>%
+    dplyr::transmute(from = .data$status_id, to = .data$favorited_by, user_id = .data$favorited_by, .data$screen_name) %>%
+    # dplyr::add_count(.data$to, name = "n_likes") %>%
+    dplyr::mutate(type = "like")
+
+
+  retweet_edges <- x$df_retweets %>%
+    dplyr::filter(.data$is_retweet) %>%
+    dplyr::transmute(from = .data$retweet_status_id, to = .data$user_id, .data$user_id, .data$screen_name) %>%
+    dplyr::mutate(type = "retweet")
+
+  edges <-
+    dplyr::bind_rows(
+      tweet_edges,
+      fav_edges,
+      user_tweet_edges,
+      retweet_edges
+    ) %>%
+    dplyr::filter(.data$from != "root")
+
+  user_nodes <-
+    tibble::tibble(name =
+                     c(df_root$user_id,
+                       user_tweet_edges$user_id) %>%
+                     unique(),
+                   type = "user") %>%
+    dplyr::left_join(df %>% dplyr::select(name = .data$user_id, .data$screen_name) %>% dplyr::distinct()) %>%
+    dplyr::mutate(url = glue::glue("https://twitter.com/{screen_name}/"))
+
+  tweet_nodes <-
+    tibble::tibble(name =
+                     c(tweet_edges$to,
+                       tweet_edges$from) %>%
+                     unique(),
+                   type = "tweet") %>%
+    dplyr::left_join(df %>% dplyr::select(name = .data$status_id, .data$screen_name, .data$text) %>% dplyr::distinct()) %>%
+    dplyr::mutate(url = glue::glue("https://twitter.com/fake_screen_name/status/{name}"))
+  # the correct url would be:
+  # dplyr::mutate(url = glue::glue("https://twitter.com/{screen_name}/status/{name}"))
+  # however, this probably wouldn't be completely inline with the twitter terms of use...
+  # (twitter will correct for the correct screen_name if the tweet is still available)
+  nodes <-
+    dplyr::full_join(user_nodes,
+                     tweet_nodes) %>%
+    dplyr::mutate(label = dplyr::coalesce(.data$text, .data$screen_name))
+
+  # dirty hack to prevent error:
+  #"
+  # Error in (function (edges, n = max(edges), directed = TRUE)  :
+  #             At type_indexededgelist.c:116 : cannot create empty graph with negative number of vertices, Invalid value
+  #"
+  xxx <- unique(nodes$name)
+  yyy <- unique(c(edges$from, edges$to))
+  ww <- dplyr::setdiff(yyy, xxx)
+  oo <- edges %>% dplyr::filter(.data$from %in% ww | .data$to %in% ww)
+  edges <- edges %>%
+    dplyr::anti_join(oo)
+  g <- tidygraph::tbl_graph(
+    nodes %>%
+      # TODO: remove row with root
+      tidyr::drop_na(.data$screen_name),
+    edges
+  )
   class(g) <- c("rtweettree_tbl_graph", "tbl_graph", "igraph")
   g
 }
 
+
+
+
+
 #' @export
+#' @describeIn rtweettree_tbl_graph return rtweettree_tbl_graph object as is.
 rtweettree_tbl_graph.rtweettree_tbl_graph <- function(x, ...) {
   x
 }
-
 
 # TODO: add example in docs how to add dist_to_center to tbl_graph object:
 # g <- g %>%
 # # calculate the distance to the tree root with tidygraph:
 #   dplyr::mutate(dist_to_center = tidygraph::node_distance_from(tidygraph::node_is_source()))
 #' @export
-rtweettree_tbl_graph.list <- rtweettree_tbl_graph.character <- function(x, ...) {
+#' @describeIn rtweettree_tbl_graph First run rtweettree_data on the status id `x` and then transform to rtweettree tbl_graph.
+rtweettree_tbl_graph.character <- function(x, ...) {
   l <- rtweettree_data(x)
   g <-
     rtweettree_tbl_graph.rtweettree_data(l)
 }
+
+#' @export
+#' @describeIn rtweettree_tbl_graph First run rtweettree_data on the named list `x` and then transform to rtweettree tbl_graph.
+rtweettree_tbl_graph.list <- rtweettree_tbl_graph.character
+
 #' @export
 rtweettree_tbl_graph.default <- function(x, ...) {
   stop("rtweettree_tbl_graph() not defined for object of class ", class(x))
